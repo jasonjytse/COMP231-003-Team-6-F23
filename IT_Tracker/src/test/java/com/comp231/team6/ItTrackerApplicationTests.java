80335df7ce297e0906b9dff7d3082f82283e718b
Jason Tse <59128576+jasonjytse@users.noreply.github.com>
2023-11-06 12:34:08 -0500

    SpringBoot Initial Commit

---
+++ IT_Tracker/src/test/java/com/comp231/team6/ItTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp231.team6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
